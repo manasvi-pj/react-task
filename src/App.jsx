47594cfac1775cc2fe80c0fa0b94a96588638c9e
manasvi-pj <manasvipunjabi18@gmail.com>
2025-03-22 15:57:47 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import ProgressBar from './components/ProgressBar/ProgressBar';
+
+function App() {
+  return (
+    <div>
+      <h1>Progress Bar</h1>
+      <ProgressBar />
+    </div>
+  );
+}
+
+export default App;
